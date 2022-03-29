fbd1f942624fac655c86a9210fa8cf2115ae277b
Zach Neill <neillz@berea.edu>
2022-03-29 01:29:47 -0400

    Machine name lines up with supply

--- src/app/supply-display/supply-display.component.ts
+++ src/app/supply-display/supply-display.component.ts
@@ -15,7 +15,6 @@ export class SupplyDisplayComponent implements OnInit {
   supply!: Supply;
   testSupply!: Supply;
   loaded: boolean;
-  @Input() machineName!:string;
   @Input() machineId!: Number;
   @Input() coffeeChecked!: boolean;
   @Input() shortSupplyChecked!: boolean;
@@ -28,6 +27,7 @@ export class SupplyDisplayComponent implements OnInit {
   ngOnInit(): void {
     this.supplyService.getSupply(+this.machineId).subscribe(data => {
       this.supply = data;
+      console.log(this.supply);
       this.supply.has_coffee ? this.coffeeChecked = true : this.coffeeChecked = false;
       this.supply.has_short_supply ? this.shortSupplyChecked = true : this.shortSupplyChecked = false;
       if (data){
@@ -43,8 +43,7 @@ export class SupplyDisplayComponent implements OnInit {
     this.testSupply.has_short_supply = false;
     this.testSupply.machine = this.supply.machine;
     this.testSupply.user = "Jim";
-    const date = new Date();
-    this.testSupply.time_checked = date;
+    this.testSupply.time_checked = new Date();
 
     this.supplyService.save(this.testSupply);
   }
