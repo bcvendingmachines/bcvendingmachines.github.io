07a433d4e4c61f6a64cd87663ab7b900dfc6f719
Zach Neill <zacharyneill@gmail.com>
2022-08-17 16:45:30 -0400

    Attempt to implement request restriction

---
+++ src/main/java/com/springapi/bcvm/RequestThrottleFilter.java
@@ -0,0 +1,77 @@
+package com.springapi.bcvm;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@ComponentScan
+public class RequestThrottleFilter implements Filter {
+
+    private final LoadingCache<String, Integer> requestCountsPerIpAddress;
+
+    public RequestThrottleFilter(){
+        super();
+        requestCountsPerIpAddress = Caffeine.newBuilder().
+                expireAfterWrite(2, TimeUnit.SECONDS).build(key -> 0);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        String clientIpAddress = getClientIP((HttpServletRequest) servletRequest);
+        if(isMaximumRequestsPerSecondExceeded(clientIpAddress)){
+            httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            httpServletResponse.getWriter().write("Too many requests");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isMaximumRequestsPerSecondExceeded(String clientIpAddress){
+        Integer requests;
+        requests = requestCountsPerIpAddress.get(clientIpAddress);
+        if(requests != null){
+            int MAX_REQUESTS_PER_SECOND = 1;
+            if(requests > MAX_REQUESTS_PER_SECOND) {
+                requestCountsPerIpAddress.asMap().remove(clientIpAddress);
+                requestCountsPerIpAddress.put(clientIpAddress, requests);
+                return true;
+            }
+
+        } else {
+            requests = 0;
+        }
+        requests++;
+        requestCountsPerIpAddress.put(clientIpAddress, requests);
+        return false;
+    }
+
+    public String getClientIP(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null){
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
