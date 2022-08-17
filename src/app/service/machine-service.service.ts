76186f26f44d6d72ca4bdb1c9026aa05d935ce4e
Zach Neill <zacharyneill@gmail.com>
2022-08-17 13:12:13 -0400

    Changed configs and form requirements

--- src/app/service/machine-service.service.ts
+++ src/app/service/machine-service.service.ts
@@ -2,24 +2,13 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from "@angular/common/http"
 import { Observable } from 'rxjs'
 import { Machine } from '../model/machine'
+import {Configs} from "./configs";
 @Injectable({
   providedIn: 'root'
 })
 export class MachineService {
-  private readonly machinesUrl: string
-  private readonly saveUrl: string
 
-  constructor(private http: HttpClient) {
-    //PRODUCTION URLs
-    // this.machinesUrl = 'https://bcvm.herokuapp.com/machines'
-    // this.saveUrl = 'https://bcvm.herokuapp.com/saveMachines'
-    //DEVELOPMENT URLs
-    this.machinesUrl = 'http://localhost:8080/machines'
-    this.saveUrl = "http://localhost:8080/saveMachines"
-  }
+  constructor(private http: HttpClient) { }
 
-  public getMachines(): Observable<Machine[]> {return this.http.get<Machine[]>(this.machinesUrl)}
-
-  public saveMachines(machine: Machine) {
-    return this.http.post(this.saveUrl, machine)}
+  public getMachines(): Observable<Machine[]> {return this.http.get<Machine[]>(Configs.machinesUrl)}
 }
--- src/app/service/supply-service.service.ts
+++ src/app/service/supply-service.service.ts
@@ -2,24 +2,15 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from "@angular/common/http"
 import { Observable } from 'rxjs'
 import { Supply } from '../model/supply'
+import {Configs} from "./configs";
 @Injectable({
   providedIn: 'root'
 })
 export class SupplyService {
-  private readonly supplyUrl: string
-  private readonly saveUrl: string
 
-  constructor(private http: HttpClient) {
-    //PRODUCTION URLs
-    // this.supplyUrl = 'https://bcvm.herokuapp.com/supply/'
-    // this.saveUrl = 'https://bcvm.herokuapp.com/save'
+  constructor(private http: HttpClient) { }
 
-    //DEVELOPMENT URLs
-    this.supplyUrl = "http://localhost:8080/supply/"
-    this.saveUrl = "http://localhost:8080/save"
-  }
+  public getSupply(id:number) : Observable<Supply> { return this.http.get<Supply>(Configs.supplyUrl + id) }
 
-  public getSupply(id:number) : Observable<Supply> { return this.http.get<Supply>(this.supplyUrl + id) }
-
-  public save(supply: Supply): Observable<Supply> { return this.http.post<Supply>(this.saveUrl, supply) }
+  public save(supply: Supply): Observable<Supply> { return this.http.post<Supply>(Configs.saveUrl, supply) }
 }
