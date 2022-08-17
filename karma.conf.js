aac1ea49f2e241cf1cca2ca9ab063feec2c74647
Zach Neill <zacharyneill@gmail.com>
2022-08-17 13:12:13 -0400

    Changed configs and form requirements

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bc-vending-machines'),
+      dir: require('path').join(__dirname, './coverage/bcvm'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/app/service/configs.ts
@@ -0,0 +1,15 @@
+// Comment out the config you don't need
+
+// PRODUCTION / HTTPS
+export enum Configs {
+  saveUrl = 'https://bcvm.herokuapp.com/save',
+  supplyUrl = 'https://bcvm.herokuapp.com/supply/',
+  machinesUrl = 'https://bcvm.herokuapp.com/machines',
+}
+
+// LOCAL / HTTP
+// export enum Configs {
+//   saveUrl = 'http://localhost:8080/save',
+//   supplyUrl = 'http://localhost:8080/supply/',
+//   machinesUrl = 'http://localhost:8080/machines',
+// }
