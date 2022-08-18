74274c2c963f68c633f75dedba743756f8c3d0a8
Zach Neill <zacharyneill@gmail.com>
2022-08-17 23:32:28 -0400

    Small improvements

--- src/main/java/com/springapi/bcvm/RouteController.java
+++ src/main/java/com/springapi/bcvm/RouteController.java
@@ -1,7 +1,13 @@
 package com.springapi.bcvm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
